10f68f1be200f401553ee7e57997f478005bdac9
halrobison <halrobisoncreative@gmail.com>
2024-03-07 21:32:46 -0600

    planning notes and comments

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ window.addEventListener("load", function() {
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        // call addDesitination - which will utilize fetch
     })
-    
+
+    // add eventListener for button
+    // then, with button, grab the 4 inputs (pilot, copilot, etc)
+    // finally, call  formSubmission - which scriptHelper has!
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,8 +2,10 @@
 
 require('cross-fetch/polyfill');
 
+// task 3
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
+    // when you uncomment this, you may want to return as a template literal - bc think about this being inserted into HTML. will use `` surrounding.
     /*
                  <h2>Mission Destination</h2>
                  <ol>
@@ -16,25 +18,45 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  <img src="">
     */
  }
- 
+
+ // task 2
  function validateInput(testInput) {
-    
- }
+    // will be shorter than formSubmission, just validating the inputs
+    // review form validation lesson in text (can copy/paste!)
+    // something like: 
+    // if (testInput[name] = pilot) {
+
+    // }
+    // will return the given phrases AS STRINGS
+    // this function will be used in formSubmission
+    // when the first part of the function is complete, should pass 2 tests 
+    // return "Empty", "Not a number", etc etc 
+}
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
+    // will be largest part of code - will be function called upon submission
+    // this is where if statements will live - enough fuel? cargo too large? etc
+    // remember, validation will utilize the validateInput function!
+
+    // when you need to change text on pilotStatus, you'll have to change the style to visible AND change the text with .innerHTML!
+    // for ex: pilotStatus = document.getElementById("pilotstatus"), then pilotstatus.innerHTML, etc etc 
+}
+
+ // task 3
  async function myFetch() {
      let planetsReturned;
  
      planetsReturned = await fetch().then( function(response) {
+        // where you will fetch all planets an as array 
          });
  
      return planetsReturned;
  }
  
  function pickPlanet(planets) {
+    // let planet = {} (bc it will be an object)
+    // where random number generator will select planet from planetsReturned array; 
+    // return planet; 
  }
  
  module.exports.addDestinationInfo = addDestinationInfo;
